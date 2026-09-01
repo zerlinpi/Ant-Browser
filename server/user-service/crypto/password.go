c789e60629906e5ea4ab07bc67c1372494ec8c93
zerlinpi <105773292+zerlinpi@users.noreply.github.com>
2026-09-01 09:12:46 +0800

    feat: add password hashing utility for user authentication

---
+++ server/user-service/crypto/password.go
@@ -0,0 +1,12 @@
+package crypto
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func ComparePassword(hash string, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
